977cad2c50c3213147e7a0aa8a092486995e6f56
Paweł Felcyn <pawfel1@st.amu.edu.pl>
2022-10-17 14:33:16 +0200

    Get Command - not finished

--- MeasuringStations/MainViewModel.cs
+++ MeasuringStations/MainViewModel.cs
@@ -1,17 +1,54 @@
 ﻿using MeasuringStations.Models;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MeasuringStations
 {
-    public class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
         [ObservableProperty]
+        private bool _isBusy;
+        [ObservableProperty]
         private Station _station;
-        
+        [ObservableProperty]
+        private int _id = 291;
+
+        [ICommand]
+        private async Task GetStationAsync()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            try
+            {
+                //IsBusy = true;
+                using var client = new HttpClient();
+                var response = await client.GetAsync("https://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/" + Id);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    Station = JsonConvert.DeserializeObject<Station>(json);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
