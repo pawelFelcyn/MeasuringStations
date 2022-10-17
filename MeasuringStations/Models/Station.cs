dbf6d25f6d609544269b048a02d430f6c43b75af
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-10-17 20:39:35 +0200

    Rename prop

--- MeasuringStations/Models/Station.cs
+++ MeasuringStations/Models/Station.cs
@@ -9,7 +9,7 @@ namespace MeasuringStations.Models
     public class Station
     {
         public int Id { get; set; }
-        public string StationsName { get; set; }
+        public string StationName { get; set; }
         public decimal GegrLat { get; set; }
         public decimal GegrLon { get; set; }
         public City City { get; set; }
