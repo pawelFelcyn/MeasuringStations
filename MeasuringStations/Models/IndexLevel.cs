f71f1ace81440dd258ae013f79d1746ceae3d77a
Paweł Felcyn <pawfel1@st.amu.edu.pl>
2022-10-17 14:16:50 +0200

    Create models and view model

---
+++ MeasuringStations/Models/IndexLevel.cs
@@ -0,0 +1,8 @@
+﻿namespace MeasuringStations.Models
+{
+    public class IndexLevel
+    {
+        public int Id { get; set; }
+        public string IndexLevelName { get; set; }
+    }
+}
---
+++ MeasuringStations/Models/Station.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasuringStations.Models
+{
+
+    public class Station
+    {
+        public int Id { get; set; }
+        public DateTime? StCalcDate { get; set; }
+        public IndexLevel StIndexLevel { get; set; }
+        public DateTime? StSourceDataDate { get; set; }
+        public DateTime? So2CalcDate { get; set; }
+        public IndexLevel So2IndexLevel { get; set; }
+        public DateTime? So2SourceDataDate { get; set; }
+        public DateTime? No2CalcDate { get; set; }
+        public IndexLevel No2IndexLevel { get; set; }
+        public DateTime? No2SourceDataDate { get; set; }
+        public DateTime? Pm10CalcDate { get; set; }
+        public IndexLevel Pm10IndexLevel { get; set; }
+        public DateTime? Pm10SourceDataDate { get; set; }
+        public DateTime? Pm25CalcDate { get; set; }
+        public IndexLevel Pm25IndexLevel { get; set; }
+        public DateTime? Pm25SourceDataDate { get; set; }
+        public DateTime? O3CalcDate { get; set; }
+        public IndexLevel O3IndexLevel { get; set; }
+        public DateTime? O3SourceDataDate { get; set; }
+        public bool StIndexStatus { get; set; }
+        public string StIndexCrParam { get; set; }
+    }
+}
