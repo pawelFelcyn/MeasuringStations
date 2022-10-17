62eed435aaf6b546d7a90a49c74d22c79a4e1960
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-10-17 19:51:09 +0200

    Create Method for geting all stations

--- MeasuringStations/MainViewModel.cs
+++ MeasuringStations/MainViewModel.cs
@@ -4,11 +4,13 @@ using Microsoft.Toolkit.Mvvm.Input;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MeasuringStations
 {
@@ -20,6 +22,12 @@ namespace MeasuringStations
         private StationDetails _station;
         [ObservableProperty]
         private int _id = 291;
+        public ObservableCollection<Station> AllStations { get; }
+
+        public MainViewModel()
+        {
+            AllStations = new();
+        }
 
         [ICommand]
         private async Task GetStationAsync()
@@ -44,6 +52,45 @@ namespace MeasuringStations
             {
 
                 throw;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public async Task GetAllStationsAsync()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            try
+            {
+                IsBusy = true;
+                AllStations.Clear();
+
+                using var client = new HttpClient();
+                var response = await client.GetAsync("https://api.gios.gov.pl/pjp-api/rest/station/findAll");
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    MessageBox.Show("Couldn't load stations.");
+                    return;
+                }
+
+                var json = await response.Content.ReadAsStringAsync();
+                var stations = JsonConvert.DeserializeObject<IEnumerable<Station>>(json);
+
+                foreach (var station in stations)
+                {
+                    AllStations.Add(station);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Couldn't load stations.");
             }
             finally
             {
